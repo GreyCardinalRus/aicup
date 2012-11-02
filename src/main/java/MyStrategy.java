54959cf3e828287564f39c524170480cee51198f
valentin <valentin@localhost>
2012-11-03 00:53:21 +0400

    ????? ????? ???? ?????? -???? ?????? ? ??????? ?????, ??????????, ?????? ? "?????" ?????????- ????? ???????? ????????

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -4,6 +4,7 @@ import static java.lang.StrictMath.PI;
 
 public final class MyStrategy implements Strategy {
 	int numStrategy = 0;
+	long tid=0;
 
 	@Override
 	public void move(Tank self, World world, Move move) {
@@ -11,7 +12,7 @@ public final class MyStrategy implements Strategy {
 		Bonus[] bonuses = world.getBonuses();
 		Player[] players = world.getPlayers();
 		// Shell[] shells = world.getShells();
-
+		//System.out.println("players="+players.length);
 		double minAngle = 3.0;
 		int i, j;
 		Player MaxRatePlayer = null;
@@ -32,48 +33,56 @@ public final class MyStrategy implements Strategy {
 
 		for (i = 0; i < tanks.length; i++)
 			// Ищем Игрока с максимальным количество очков
-			if (self != tanks[i] && !tanks[i].isTeammate()
-					&& tanks[i].getCrewHealth() > 0
-					&& tanks[i].getHullDurability() > 0) {
+			if (!tanks[i].isTeammate() // Enemy!!
+					&& tanks[i].getCrewHealth() > 0 // live!!!
+					&& tanks[i].getHullDurability() > 0 // live!!!
+			)
 				for (j = 0; j < players.length; j++) {
-					if (MaxRatePlayer == null
-							|| players[j].getScore() > MaxRatePlayer.getScore()) {
+					if (tanks[i].getPlayerName().equals(players[j].getName()) &&(
+					MaxRatePlayer == null
+							|| players[j].getScore() > MaxRatePlayer.getScore()))
+							{
 						MaxRatePlayer = players[j];
-					}
-				}
-			}
-		if (MaxRatePlayer == null || MaxRatePlayer.getScore() == 0) {
-			MaxRatePlayer = null;
-			for (i = 0; i < tanks.length; i++) {
-				if (!tanks[i].isTeammate() && tanks[i].getCrewHealth() > 0
-						&& tanks[i].getHullDurability() > 0) {
-					if (minAngle > self.getTurretAngleTo(tanks[i])) {
 						tank = tanks[i];
-						minAngle = self.getTurretAngleTo(tanks[i]);
+						//System.out.println("Players="+MaxRatePlayer.getScore());
 					}
-				}
-			}
-		} else {
-			for (i = 0; i < tanks.length; i++) {
-				if (tanks[i].getPlayerName().equals(MaxRatePlayer.getName())) {
-					tank = tanks[i];
-					// minAngle = self.getTurretAngleTo(tanks[i]);
-				}
-			}
 
-		}
+				}
+//		if (MaxRatePlayer == null || MaxRatePlayer.getScore() == 0) {
+//			MaxRatePlayer = null;
+//			for (i = 0; i < tanks.length; i++) {
+//				if (!tanks[i].isTeammate() && tanks[i].getCrewHealth() > 0
+//						&& tanks[i].getHullDurability() > 0) {
+//					if (minAngle > self.getTurretAngleTo(tanks[i])) {
+//						tank = tanks[i];
+//						minAngle = self.getTurretAngleTo(tanks[i]);
+//					}
+//				}
+//			}
+//		} else {
+////			for (i = 0; i < tanks.length; i++) {
+////				if (tanks[i].getPlayerName().equals(MaxRatePlayer.getName())) {
+////					tank = tanks[i];
+////				 minAngle = self.getTurretAngleTo(tanks[i]);
+////				}
+////			}
+//
+//		}
 		if (tank != null) {
+			if(tid != tank.getId())System.out.println("change Target:" +tank.getId());
+			tid = tank.getId(); 
 			minAngle = self.getTurretAngleTo(tank);
 			double dist = self.getDistanceTo(tank);
 
-			if (minAngle < (1 / dist) && minAngle > (-1 / dist)
+			if (minAngle < (2 / dist) && minAngle > (-2 / dist)
 					&& (self.getRemainingReloadingTime() == 0))
 				move.setFireType(FireType.PREMIUM_PREFERRED);
 			else
 				move.setTurretTurn(minAngle);
-		}
+		}else {System.out.println("lost Target:");move.setTurretTurn(1);}
+		
 		if (self.getRemainingReloadingTime() < 10)
-			moveAngel = minAngle; // если можем стрелять - повернемся всем
+			moveAngel = 2*minAngle; // если можем стрелять - повернемся всем
 									// корпусом!
 		if (moveAngel > 2) {
 			move.setLeftTrackPower(-1);
@@ -96,8 +105,8 @@ public final class MyStrategy implements Strategy {
 
 	@Override
 	public TankType selectTank(int tankIndex, int teamSize) {
-		if (1 == teamSize)
-			return TankType.MEDIUM;
+		//if (1 == teamSize)
+		//	return TankType.HEAVY;
 		return TankType.MEDIUM;
 	}
 
